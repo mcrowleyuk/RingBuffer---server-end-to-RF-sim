9369fe77a8fe91757dae99ccbcd7ca52377e2293
Martin Crowley <martincrowley99@outlook.com>
2025-10-28 14:07:03 +0000

    verfied receive buffer working as expected

--- CommunicationSystem.h
+++ CommunicationSystem.h
@@ -13,6 +13,7 @@
 // Communication System using UDP
 class CommunicationSystem {
 public:
+    
     CommunicationSystem(const std::string& ip, uint16_t port) : send_buffer(1024), receive_buffer(1024), ip(ip), port(port) {
         if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
             std::cerr << "WSAStartup failed" << std::endl;
